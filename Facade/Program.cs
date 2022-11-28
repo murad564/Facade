7bad1ca2fd99ddf49a68ae8bcc1a103e5c773eba
Mehe_od76@itstep.edu.az <Mehe_od76@itstep.edu.az>
2022-11-28 13:55:29 +0400

    Add project files.

---
+++ Facade/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Facade;
+
+
+public interface Shape
+{
+    void draw();
+}
+public class Rectangle : Shape
+{
+    public void draw()
+    {
+        Console.WriteLine("Rectangle => draw()");
+    }
+}
+
+public class Square : Shape
+{
+
+    public void draw()
+    {
+        Console.WriteLine("Square => draw()");
+    }
+}
+
+public class Circle : Shape
+{
+   public void draw()
+    {
+        Console.WriteLine("Circle => draw()");
+    }
+}
+
+
+public class ShapeMaker
+{
+    private Shape circle;
+    private Shape rectangle;
+    private Shape square;
+
+    public ShapeMaker()
+    {
+        circle = new Circle();
+        rectangle = new Rectangle();
+        square = new Square();
+    }
+
+    public void drawCircle()
+    {
+        circle.draw();
+    }
+    public void drawRectangle()
+    {
+        rectangle.draw();
+    }
+    public void drawSquare()
+    {
+        square.draw();
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        ShapeMaker shapeMaker = new ShapeMaker();
+
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+    }
+}
